7280c84c1b215d1023aeb3bd7e0fb2b7bd94b821
lexvenim <lexvenim@gmail.com>
2018-01-01 21:21:42 +0300

    Better header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+import { AuthGuard } from './common/auth.guard';
+
+import { AuthService } from './services/auth.service';
+import { RoutingService } from './services/routing.service';
+import { UserService } from './services/user.service';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+	constructor(private router: RoutingService,
+		public auth: AuthService,
+		public user: UserService,
+		private aguard: AuthGuard) { }
+
+	logout(){
+		this.router.home()
+		this.auth.logout()
+	}
+
+	goTo(page){
+		this.router.go(page)
+	}
+
+	home(){
+		this.router.home()
+	}
+
+	back(){
+		this.router.back()
+	}
+
+	now(){
+		return this.router.now()
+	}
 }
